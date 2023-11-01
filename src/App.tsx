0ca091a4db5a6b7a686f12f71a93c0efabd33037
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2023-11-01 23:34:22 +0100

    Initiate the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import ListGroup from "../components/Listgroup";
-import Alert from "../components/Alert";
-import Button from "../components/Button";
+import Form from "../components/Form";
 import { useState } from "react";
 
-let cities = ["Rabat", "Casa", "Grenoble", "Paris"];
-
 function App() {
-  const [alertVisibility, setAlertVisibility] = useState(false);
   return (
-    <div>
-      <ListGroup
-        cities={cities}
-        heading="Cities"
-        onSelectItem={(city: string) => console.log(city)}
-      />
-      {alertVisibility && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Hehe ha Alerte khdama
-        </Alert>
-      )}
-      <Button
-        onClick={() => {
-          setAlertVisibility(true);
-        }}
-      >
-        My Button
-      </Button>
-    </div>
+    <>
+      <Form />
+    </>
   );
 }
 
