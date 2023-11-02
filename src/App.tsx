c9e4df5ce89100228fc0a5f928285b98432352de
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2023-11-02 14:16:18 +0100

    Adding Expenses List & Filter Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import Form from "../components/Form";
+import Form from "./components/Form";
 import { useState } from "react";
+import ExpensesList from "./expenses-tracker/ExpensesList";
+import ExpenseFilter from "./expenses-tracker/ExpenseFilter";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 25,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 15,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 20,
+      category: "Utilities",
+    },
+  ]);
+
+  const [selectedCategory, setSelectedCategory] = useState(" ");
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  console.log(visibleExpenses);
+
+  if (expenses.length == 0) return null;
   return (
     <>
-      <Form />
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpensesList
+        expenses={visibleExpenses}
+        onDelete={(id) =>
+          setExpenses(expenses.filter((expense) => expense.id !== id))
+        }
+      />
     </>
   );
 }
